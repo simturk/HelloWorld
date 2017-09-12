98458d6c1380413d286b848292a539d9916053cd
simturk <the_simi@web.de>
2017-09-12 17:03:13 +0200

    second commit second try

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,12 @@
 
 int main(){
 
-printf("Hello World!\n");
-printf("Little human here.\n");
+int number;
+
+	printf("Hello World!\n");
+	printf("Little human here.\n");
+	printf("Give number a number please\n");
+	scanf("%d", &number);
+	printf("The number was: %d\n", number);
 
 }
